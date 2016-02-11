cd15f282cf6fdd4d5eeb0cc6a7552a5e89daf905
Kyle McCarthy <km@kylemccarthy.io>
2016-02-10 20:14:21 -0600

    checkerBoard initalization

--- src/kjmd54checkers/Kjmd54Checkers.java
+++ src/kjmd54checkers/Kjmd54Checkers.java
@@ -8,6 +8,8 @@ package kjmd54checkers;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.paint.Color;
+import javafx.scene.layout.GridPane;
 
 /**
  *
@@ -15,9 +17,19 @@ import javafx.stage.Stage;
  */
 public class Kjmd54Checkers extends Application {
     
+    private final int numRows = 8;
+    private final int numCols = 8;
+    private final int boardWidth = 600;
+    private final int boardHeight = 600;
+    private final Color lightColor = Color.SKYBLUE;
+    private final Color darkColor = Color.DARKBLUE;
+    private CheckerBoard checkerBoard;
+    
     @Override
     public void start(Stage primaryStage) {
-        Scene scene = new Scene(null, 300, 250);
+        checkerBoard = new CheckerBoard(numRows, numCols, boardWidth, boardHeight, lightColor, darkColor);
+        GridPane board = checkerBoard.build();
+        Scene scene = new Scene(board, boardWidth, boardHeight);
         
         primaryStage.setTitle("Checkers");
         primaryStage.setScene(scene);
